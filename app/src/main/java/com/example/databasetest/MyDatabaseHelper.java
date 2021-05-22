386dc7a72d2eefa0bd169ba3521f56179860f321
butterkeks797 <2839645547@qq.com>
2021-05-23 01:20:56 +0800

    feat: delete some files

--- app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
@@ -52,6 +52,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Question");
         db.execSQL("drop table if exists Category");
         onCreate(db);
     }
